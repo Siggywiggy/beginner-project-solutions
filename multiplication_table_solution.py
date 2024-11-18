3938d8d27f416b1c2039ab0096906690a385afdd
Siggywiggy <kurmaadam@gmail.com>
2024-11-18 23:12:44 +0200

    work on multiplication table solution

--- multiplication_table_solution.py
+++ multiplication_table_solution.py
@@ -1,5 +1,6 @@
 #! python3
 # a program that prints oout multiplication table 1 - 9
+from tomlkit import string
 
 multiplication_range = int(input("Please enter the desired multiplication range"))
 
@@ -14,7 +15,7 @@ for i in range(1, multiplication_range + 1):
 # determine the widest numeric value in the very bottom of the table aka the highest values
 column_widths = [len(str(i)) for i in multiplication_table[-1]]
 
-# print(column_widths)
+print(column_widths)
 
 for i in range(0, multiplication_range):
     row_adjusted = str()
@@ -22,18 +23,18 @@ for i in range(0, multiplication_range):
         string_num = str(num)
         # print(f"current column is {column_widths[i]} wide")
         # print(f"number is {string_num}, {len(string_num)} wide")
-        if len(string_num) >= column_widths[i]:
-            row_adjusted += string_num
-            # print("dont need to adjust!")
-        else:
-            # print("need to adjust")
-            total_lenght = column_widths[i]
+        if len(string_num) == column_widths[i]:
+            row_adjusted += " " + string_num
+            # row_adjusted += string_num.rjust(len(string_num) + 1, " ")
+            # print(f"dont need to adjust {string_num}!")
+        if len(string_num) < column_widths[i]:
             # print(
-            #    f"need to add {column_widths[i] - len(string_num)} spaces to the left"
+            #    f"adjust {string_num} by {column_widths[i] - len(string_num)} because {column_widths[i]} is larger"
             # )
-            # adjusted_num = string_num.rjust(total_lenght, "*")
-            adjusted_num = (column_widths[i] - len(string_num)) * "x" + string_num
-            # adjusted_num = string_num.rjust(total_lenght, "x")
-            # print(f"adjusted number is {adjusted_num}")
-            row_adjusted += adjusted_num
+            row_adjusted += " " + string_num.rjust(
+                column_widths[i] - len(string_num), "x"
+            )
+
+        else:
+            print(f"somethings wrong with {len(string_num)} and {column_widths[i]}!")
     print(row_adjusted)
