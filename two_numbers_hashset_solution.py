bc9bf732216fef8e4cb9dcf939d5da3d98dcd28d
Siggywiggy <kurmaadam@gmail.com>
2025-01-12 00:42:26 +0200

    work on cProfiler, timeit

--- two_numbers_hashset_solution.py
+++ two_numbers_hashset_solution.py
@@ -3,17 +3,25 @@
 # hash set method
 # time complexity 0(n) - single iteration over the array
 # auxiliary space - O(n) - space needed for hash set
+import cProfile
+import timeit
 
-numbers = [2, 7, 11, 15, 6, 98, 75, 3, 4, 87, 1, 8, 5]
-target = 9
 
-seen_dict = dict()
+def hashset_2_sum():
+    numbers = [2, 7, 11, 15, 6, 98, 75, 3, 4, 87, 1, 8, 5]
+    target = 9
 
-for i, num in enumerate(numbers):
-    # add the number and its index to the dictionary
-    seen_dict[num] = i
-    diff = target - num
-    if diff in seen_dict.keys():
-        print(seen_dict[diff], i)
-    else:
-        continue
+    seen_dict = dict()
+
+    for i, num in enumerate(numbers):
+        # add the number and its index to the dictionary
+        seen_dict[num] = i
+        diff = target - num
+        if diff in seen_dict.keys():
+            return (seen_dict[diff], i)
+        else:
+            continue
+
+
+cProfile.run("hashset_2_sum()")
+print(timeit.timeit("hashset_2_sum()", globals=globals()))
--- two_numbers_naive_solution.py
+++ two_numbers_naive_solution.py
@@ -2,42 +2,48 @@
 # a program to find the 2 numbers from a list of numbers that add up to a target number
 # https://www.geeksforgeeks.org/check-if-pair-with-given-sum-exists-in-array/
 
+import cProfile
+import timeit
 
-numbers = [2, 7, 11, 15]
-target = 9
 
-# naive approach with memoization optimization
+def naive_sum_two_numbers():
+    numbers = [2, 7, 11, 15]
+    target = 9
 
-# memoization dictionary
-memory_dict = {}
+    # naive approach with memoization optimization
 
-for i in range(1, len(numbers) - 1):
-    # print(i)
-    for num in numbers:
-        # if the key "numbers[i] + num* exists in memory dictionary
-        # means we have already calculated it
-        if (f"{numbers[i]} + {num}") in memory_dict.keys():
-            continue
-        else:
-            # add the combination as a key and the sum and the number coordinates as a list in value
-            memory_dict[f"{num} + {numbers[i]}"] = [
-                num + numbers[i],
-                i,
-                numbers.index(num),
-            ]
-            # add the combination also in reverse as we have already calculated it
-            memory_dict[f"{numbers[i]} + {num}"] = [
-                num + numbers[i],
-                i,
-                numbers.index(num),
-            ]
-# loop trough the keys and values in the memory_dictionary
-for key, value in memory_dict.items():
-    # if the first index in the value list == target
-    if value[0] == target:
-        print(value[1], value[2])
-print(memory_dict)
+    # memoization dictionary
+    memory_dict = {}
 
+    for i in range(1, len(numbers) - 1):
+        # print(i)
+        for num in numbers:
+            # if the key "numbers[i] + num* exists in memory dictionary
+            # means we have already calculated it
+            if (f"{numbers[i]} + {num}") in memory_dict.keys():
+                continue
+            else:
+                # add the combination as a key and the sum and the number coordinates as a list in value
+                memory_dict[f"{num} + {numbers[i]}"] = [
+                    num + numbers[i],
+                    i,
+                    numbers.index(num),
+                ]
+                # add the combination also in reverse as we have already calculated it
+                memory_dict[f"{numbers[i]} + {num}"] = [
+                    num + numbers[i],
+                    i,
+                    numbers.index(num),
+                ]
+    # loop trough the keys and values in the memory_dictionary
+    for key, value in memory_dict.items():
+        # if the first index in the value list == target
+        if value[0] == target:
+            return (value[1], value[2])
+
+
+cProfile.run("naive_sum_two_numbers()")
+print(timeit.timeit("naive_sum_two_numbers", globals=globals()))
 # hash set method
 # time complexity 0(n) - single iteration over the array
 # auxiliary space - O(n) - space needed for hash set
